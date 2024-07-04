168a2ffae32142d49a4d7a45394fdfcfe3e29e65
shawon <sadmanshawonkst11@gmail.com>
2024-07-04 18:03:21 +0600

    solve 638A problem in cf(1100)

---
+++ 638A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define endl "\n"
+#define pb push_back  
+const int N=1e5+10;
+int dx[]={0,0,-1,1};
+int dy[]={-1,1,0,0};
+//int dx2[]={0,0,-1,1,1,1,-1,-1};
+//int dy2[]={-1,1,0,0,1,-1,1,-1};
+#define vi vector<int>
+#define vp vector<pair<int,int>>
+#define  mii map<int,int>
+//priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
+
+void faster()
+{
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+}
+#define testCase \
+    int t;       \
+    cin >> t;    \
+    while (t--)
+int32_t main()
+{
+     faster();
+        int a,b;
+        cin>>a>>b;
+        if(b%2==1)
+        {
+          b=a-(b-1);
+        }
+        int x=a-b;
+        x/=2;
+        cout<<(++x)<<endl;
+             
+}
