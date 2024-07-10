bb8ff90394ea2471b20a81a252b49b111df54bcc
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2024-07-10 23:22:21 +0600

    solve div3 A

---
+++ 1619A.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define endl "\n"
+#define pb push_back  
+const int N=1e5+10;
+int dx[]={0,0,-1,1};
+int dy[]={-1,1,0,0};
+//int dx2[]={0,0,-1,1,1,1,-1,-1};
+//int dy2[]={-1,1,0,0,1,-1,1,-1};
+#define vi vector<int>
+#define vp vector<pair<int,int>>
+#define  mii map<int,int>
+//priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
+
+void faster()
+{
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+}
+#define testCase \
+    int t;       \
+    cin >> t;    \
+    while (t--)
+int32_t main()
+{
+     faster();
+     testCase
+     {
+          string str;
+          cin>>str;
+          int d=str.size();
+          if(d%2==1)
+          {
+               no;
+          }
+          else
+          {
+                string s=str.substr(d/2,d/2);
+           string s2=str.substr(0,d/2);
+          if(s==s2)
+          {
+               yes;
+          }
+          else
+          {
+               no;
+          }
+          }
+     }
+             
+}
