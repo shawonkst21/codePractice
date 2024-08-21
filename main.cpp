c50556a822110f92f4842503da099d57a7a21f96
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2024-08-21 16:18:42 +0600

    learn something from this problem

--- main.cpp
+++ main.cpp
@@ -31,34 +31,61 @@ int32_t main()
      {
         int n;
         cin>>n;
-        vi v(n);
-        int sum=0;
-        for(auto &i:v)
-        {
-           cin>>i;
-           sum+=i;
-        }
 
-        bool ok =false;
-        for(int i=0;i<n-1;i++)
+        if(n%2==0)
         {
-            if(v[i]<0)
-            {
-                ok=true;
-                if(v[i+1]<0)
-                {
-                    sum+=4;
-                    break;
-                }
-
-            }
+           cout<<n/2<<' '<<n/2<<endl;
         }
-        if(!ok && v[n-1]>0)
+        else
         {
-            sum+=(-4);
-        }
-        cout<<sum<<endl;
+           int x=n/2;
+           int y=n-x;
+           if(n%20==19)
+           {
+               string s=to_string(n);
+               string a=s,b=s;
+               int val1=0,val2=0;
+               for(int i=0;i<s.size();i++)
+               {
+                    int v1=(s[i]-'0')/2;
+                    int v2=(s[i]-'0')-v1;
+                    if(v1==v2)
+                    {
+                         a[i]='0'+v1;
+                         b[i]='0'+v2;
+                    }
+                    if(v1>v2) swap(v1,v2);
+                    if(val1>=val2)
+                    {
+                         b[i]='0'+v2;
+                         a[i]='0'+v1;
+                         val1+=(a[i]-'0');
+                         val2+=(b[i]-'0');
+                    }
+                    else
+                    {
+                          b[i]='0'+v1;
+                         a[i]='0'+v2;
+                         val1+=(a[i]-'0');
+                         val2+=(b[i]-'0');
+                    }
+               }
+               reverse(a.begin(),a.end());
+                reverse(b.begin(),b.end());
+                while(a.back()=='0')a.pop_back();
 
+                while(b.back()=='0')b.pop_back();
+
+                reverse(a.begin(),a.end());
+                reverse(b.begin(),b.end());
+                cout<<a<<' '<<b<<endl;
+
+           }
+           else
+           {
+               cout<<x<<' '<<y<<endl;
+           }
+        }
      }
              
 }
