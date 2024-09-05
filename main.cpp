4d021604a18b43f915073afc5f4b5e68af39797a
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2024-09-05 09:52:09 +0600

    blablaaa

--- main.cpp
+++ main.cpp
@@ -31,70 +31,9 @@ int32_t main()
      faster();
      testCase
      {
-          int n;
-          cin>>n;
-          string str;
-          cin>>str;
-          int x=sqrt(n);
-          if(x*x!=n)
-          {
-               no;
-          }
-          else
-          {
-               bool ok1=true;
-               bool ok0=true;
-               for(int i=1;i<=x;i++)
-               {
-                    for(int j=1;j<=x;j++)
-                    {
-                         if(i==1 || i==x)
-                         {
-                              int temp=(i-1)*x+j;
-                              temp--;
-                              if(str[temp]!='1')
-                              {
-                                  ok1=false;
-                                  break;
-                              }
-                         }
-                         else if(i>1 && i<x)
-                         {
-                              if(j==1 || j==x)
-                              {
-                                   int temp=(i-1)*x+j;
-                                   temp--;
-                                   if(str[temp]!='1')
-                                 {
-                                      ok1=false;
-                                      break;
-                                 }
-                              }
-                              else
-                              {
-                                   int temp=(i-1)*x+j;
-                                   temp--;
-                                   if(str[temp]!='0')
-                                   {
-                                       ok0=false;
-                                       break;
-                                   }
-                              }
-                         }
-                    }
-                    if(ok1==false || ok0==false)
-                    {
-                         break;
-                    }
-               }
-               if(ok1==true && ok0==true)
-               {
-                    yes;
-               }
-               else
-               {
-                    no;
-               }
-          }
+         int a,b;
+         cin>>a>>b;
+         int mx=max(a,b);
+         cout<<(mx-a)+(b-mx)<<endl;
      } 
 }
