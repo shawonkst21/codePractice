e4a2e59de8afb1f7287c6544fd5158e80af21ed8
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2025-01-07 00:46:24 +0700

    day6

--- leetCode.cpp
+++ leetCode.cpp
@@ -1,41 +1,50 @@
 class Solution {
 public:
-    string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        int n=s.size();
-        vector<int>pre(n,0);
-        for(auto q:shifts)
-        {
-            int l=q[0];
-            int r=q[1];
-            int shift=q[2];
-            int x;
-            if(shift==0)
+    vector<int> minOperations(string boxes) {
+        int n=boxes.size();
+        vector<int>box(n,0);
+       for(int i=0;i<n;i++)
+       {
+            if(boxes[i]=='1')
             {
-                x=-1;
+                box[i]=1;
             }
-            else if(shift==1)
-            {
-                x=1;
-            }
-            pre[l]+=x;
-            if(r+1<n)
-            {
-                pre[r+1]-=x;
-            }
-        }
-        for(int i=1;i<n;i++)
-        {
-            pre[i]+=pre[i-1];
-        }
-        for(int i=0;i<n;i++)
-        {
-            int shift=pre[i]%26;
-            if(shift<0)
-            {
-                shift+=26;
-            }
-            s[i]=((s[i]-'a')+shift)%26+'a';
-        }
-        return s;
+       }
+       vector<int>temp=box;
+      for(int i=1;i<n;i++)
+      {
+        box[i]+=box[i-1];
+      }
+      for(int i=n-2;i>=0;i--)
+      {
+        temp[i]+=temp[i+1];
+      }
+      vector<int>pre(n+10,0),suf(n+10,0);
+      pre[0]=box[0];
+      for(int i=1;i<n;i++)
+      {
+        pre[i]=pre[i-1]+box[i];
+      }
+      suf[n-1]=temp[n-1];
+      for(int i=n-2;i>=0;i--)
+      {
+        suf[i]=temp[i]+suf[i+1];
+      }
+      vector<int>ans(n,0);
+      for(int i=0;i<n;i++)
+      {
+          if(i==0)
+          {
+              ans[i]=suf[i+1];
+          }
+          else if(i==n-1)
+          {
+            ans[i]=pre[i-1];
+          }
+          else{
+            ans[i]=pre[i-1]+suf[i+1];
+          }
+      }
+      return ans;
     }
 };
