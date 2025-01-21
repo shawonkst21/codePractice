ea287a11d690ba0535f5a005e498344acddfc26d
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2025-01-21 19:19:58 +0600

    day21

--- leetCode.cpp
+++ leetCode.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
-    bool doesValidArrayExist(vector<int>& derived) {
-        int n=derived.size();
-        vector<int>temp(n);
-        temp[0]=0;
-        for(int i=0;i<n;i++)
-        {
-            if(i!=n-1)
+    long long gridGame(vector<vector<int>>& g) {
+        int m=g[0].size();
+        vector<vector<long long>>grid(2,vector<long long>(m));
+        grid[0][0]=g[0][0];
+            for(int j=1;j<m;j++)
             {
-                temp[i+1]=temp[i]^derived[i];
+                grid[0][j]=g[0][j]+grid[0][j-1];
             }
-            else{
-                if(temp[0]==temp[i]^derived[i])
-                {
-                    return true;
-                }
-               
-            }
-        }
-        temp.clear();
-        temp[0]=1;
-        for(int i=0;i<n;i++)
-        {
-            if(i!=n-1)
+             grid[1][m-1]=g[1][m-1];
+             for(int j=m-2;j>=0;j--)
             {
-                temp[i+1]=temp[i]^derived[i];
+                grid[1][j]=g[1][j]+grid[1][j+1];
             }
-            else{
-                if(temp[0]==temp[i]^derived[i])
-                {
-                    return true;
-                }
-               
-            }
-        }
-        return false;
-
+           long long mx=LLONG_MAX;
+           for(int i=m-1;i>=0;i--)
+           {
+              long long sum;
+              if(i==m-1)
+              {
+                sum=grid[1][0]-grid[1][i];
+              }
+              else if(i==0)
+              {
+                 sum=grid[0][m-1]-grid[0][0];
+              }
+              else{
+                sum=max((grid[0][m-1]-grid[0][i]),(grid[1][0]-grid[1][i]));
+              }
+              mx=min(sum,mx);
+           }
+          return mx;
         
+     
     }
 };
