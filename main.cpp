cf2162cbd3ef6682e2a717a4c4c1cb7a60378910
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2025-01-22 22:56:33 +0600

    day23

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@ int dy[] = {-1, 1, 0, 0};
 #define mii map<int, int>
 #define setBits(a) (int)__builtin_popcountll(a)
 //priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
-
 void faster() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -38,6 +37,38 @@ void faster() {
 int32_t main() {
     faster();
     testCase{
+        int n;
+        cin>>n;
+        vector<vector<int>>adj(n+1);
+        vector<int>deg(n+3,0);
+        for(int i=0;i<n-1;i++)
+        {
+            int u,v;
+            cin>>u>>v;
+            adj[u].pb(v);
+            adj[v].pb(u);
+            deg[u]++;
+            deg[v]++;
+        }
+        int max1 = 0, node1 = -1;
+        for (int i = 1; i <= n; i++) {
+            if (deg[i] > max1) {
+                max1 = deg[i];
+                node1 = i;
+            }
+        }
+        deg[node1] = 0;
+        for(auto i:adj[node1])
+        {
+            deg[i]--;
+        }
+         int max2 = 0;
+        for (int i = 1; i <= n; i++) {
+            if (deg[i] > max2) {
+                max2 = deg[i];
+            }
+        }
+        cout<<(max1+max2)-1<<endl;
         
     }
 }
