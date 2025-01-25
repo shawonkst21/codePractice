5b7df3a7df1df657193a02dd9a8ca54c80beeb8f
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2025-01-25 08:10:53 +0600

    day24

--- main.cpp
+++ main.cpp
@@ -15,62 +15,56 @@ int dy[] = {-1, 1, 0, 0};
 #define mii map<int, int>
 #define setBits(a) (int)__builtin_popcountll(a)
 #define mod 1000000007
-// priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
+//priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
 
-void faster()
-{
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+void faster() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 }
 
-#define input(a)    \
-  for (auto &x : a) \
-  cin >> x
-#define output(a)       \
-  {                     \
-    for (auto &x : a)   \
-      cout << x << ' '; \
-    cout << endl;       \
-  }
+#define input(a)      \
+    for (auto &x : a) \
+        cin >> x
+#define output(a)             \
+    {                         \
+        for (auto &x : a)     \
+            cout << x << ' '; \
+        cout << endl;         \
+    }
 #define testCase \
-  int t;         \
-  cin >> t;      \
-  while (t--)
+    int t;       \
+    cin >> t;    \
+    while (t--)
 
-int32_t main()
-{
-  faster();
-  int n;
-  cin >> n;
-  vi a(n + 1);
-  for (int i = 1; i <= n; i++)
-    cin >> a[i];
-  vector<vi> dp(n + 2, vi(n + 2, 0));
-  for (int i = n; i >= 1; i--)
-  {
-    for (int j = i; j <= n; j++)
-    {
-      if (i == j)
-      {
-        dp[i][j] = 1;
-      }
-      else
-      {
-        dp[i][j] = 1 + dp[i + 1][j];
-        if (a[i] == a[i + 1])
-        {
-          dp[i][j] = min(dp[i][j], 1 + dp[i + 2][j]);
+int32_t main() {
+    faster();
+    testCase{
+        int n;
+        cin>>n;
+        stack<int>s;
+        for(int i=0;i<n;i++){
+          s.push(i*i);
         }
-
-        for (int k = i + 2; k <= j; k++)
+        vector<int>ans(n,-1);
+        for(int i=n-1;i>=0;)
         {
-          if (a[i] == a[k])
+          if(ans[i]==-1)
           {
-            dp[i][j] = min(dp[i][j], dp[i + 1][k - 1] + dp[k + 1][j]);
+              if(s.top()-i>i)
+              {
+                s.pop();
+              }
+              else{
+                int x=s.top()-i;
+                ans[x]=i;
+                ans[i]=x;
+                i--;
+              }
+          }
+          else{
+            i--;
           }
         }
-      }
+        output(ans);
     }
-  }
-  cout << dp[1][n] << endl;
 }
