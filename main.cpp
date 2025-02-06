597e25cc57ca645ca91348383fcb01767dc18430
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2025-02-06 22:34:26 +0600

    day6

--- main.cpp
+++ main.cpp
@@ -1,89 +1,90 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
 #define endl "\n"
+#define pb push_back
+const int N = 1e5 + 10;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+// int dx2[]={0,0,-1,1,1,1,-1,-1};
+// int dy2[]={-1,1,0,0,1,-1,1,-1};
 #define vi vector<int>
-#define N 100010
+#define vp vector<pair<int, int>>
+#define mii map<int, int>
+#define setBits(a) (int)__builtin_popcountll(a)
+#define mod 1000000007
+//priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
 
 void faster() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 }
 
-int n, q;
-int a[N];
-
-struct SegmentTree {
-    #define lc (node << 1)
-    #define rc ((node << 1) + 1)
-
-    int t[4 * N];
-
-    SegmentTree() {
-        memset(t, 0, sizeof(t));
+#define input(a)      \
+    for (auto &x : a) \
+        cin >> x
+#define output(a)             \
+    {                         \
+        for (auto &x : a)     \
+            cout << x << ' '; \
+        cout << endl;         \
     }
-
-    // Function to determine the level operation (AND or OR)
-    bool isAnd(int depth) {
-        return (depth % 2 == (n % 2 == 0 ? 1 : 0));  // Ensuring root follows correct operation
-    }
-
-    // Combine function based on the depth (AND or OR operation)
-    int combine(int left, int right, int depth) {
-        return isAnd(depth) ? (left & right) : (left | right);
-    }
-
-    // Build the segment tree
-    void build(int node, int start, int end, int depth) {
-        if (start == end) {
-            t[node] = a[start];
-        } else {
-            int mid = (start + end) >> 1;
-            build(lc, start, mid, depth + 1);
-            build(rc, mid + 1, end, depth + 1);
-            t[node] = combine(t[lc], t[rc], depth);
-        }
-    }
-
-    // Point update in segment tree
-    void update(int node, int start, int end, int idx, int value, int depth) {
-        if (start == end) {
-            t[node] = value;
-        } else {
-            int mid = (start + end) >> 1;
-            if (idx <= mid)
-                update(lc, start, mid, idx, value, depth + 1);
-            else
-                update(rc, mid + 1, end, idx, value, depth + 1);
-            t[node] = combine(t[lc], t[rc], depth);
-        }
-    }
-
-    // Query root to get the current beauty value
-    int getBeauty() {
-        return t[1];  // The root node contains the final computed value
-    }
-
-} segTree;
+#define testCase \
+    int t;       \
+    cin >> t;    \
+    while (t--)
 
 int32_t main() {
     faster();
-    int t;
-    cin >> t;
-    while (t--) {
-        cin >> n;
-        for (int i = 1; i <= n; i++) cin >> a[i];
-
-        // Build segment tree
-        segTree.build(1, 1, n, 0);
-
-        cin >> q;
-        while (q--) {
-            int p, x;
-            cin >> p >> x;
-            segTree.update(1, 1, n, p, x, 0);
-            cout << segTree.getBeauty() << endl;
+    testCase{
+        int n;
+        cin>>n;
+        vi v(n);
+        map<int,vector<int>>mp;
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
+            mp[v[i]].pb(i);
+        }
+        vector<vector<int>>v1;
+        for(auto i:mp)
+        {
+            v1.pb(i.second);
+        }
+        sort(begin(v1),end(v1));
+        int q;
+        cin>>q;
+        while(q--)
+        {
+            string s;
+            cin>>s;
+            if(s.size()!=n)
+            {
+                no;
+                continue;
+            }
+            else{
+                map<char,vector<int>>m2;
+                for(int i=0;i<n;i++)
+                {
+                    m2[s[i]].pb(i);
+                }
+                vector<vector<int>>v2;
+                for(auto i:m2)
+                {
+                    v2.pb(i.second);
+                }
+                sort(begin(v2),end(v2));
+                if(v1==v2)
+                {
+                    yes;
+                }
+                else{
+                    no;
+                }
+            }
         }
     }
-    return 0;
 }
