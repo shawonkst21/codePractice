52f7f46f581ce28651c21e72c15631c0cafc6e9d
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2025-02-19 22:42:10 +0600

    da

--- main.cpp
+++ main.cpp
@@ -5,70 +5,59 @@ using namespace std;
 #define no cout << "NO" << endl
 #define endl "\n"
 #define pb push_back
-#define MOD 998244353
+const int N = 1e5 + 10;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+// int dx2[]={0,0,-1,1,1,1,-1,-1};
+// int dy2[]={-1,1,0,0,1,-1,1,-1};
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mii map<int, int>
+#define setBits(a) (int)__builtin_popcountll(a)
+#define mod 1000000007
+//priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
 
 void faster() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 }
 
-#define input(a) for (auto &x : a) cin >> x
-#define output(a) { for (auto &x : a) cout << x << ' '; cout << endl; }
-#define testCase int t; cin >> t; while (t--)
-
-int power(int base, int exp, int mod) {
-    int result = 1;
-    while (exp > 0) {
-        if (exp % 2) result = (result * base) % mod;
-        base = (base * base) % mod;
-        exp /= 2;
+#define input(a)      \
+    for (auto &x : a) \
+        cin >> x
+#define output(a)             \
+    {                         \
+        for (auto &x : a)     \
+            cout << x << ' '; \
+        cout << endl;         \
     }
-    return result;
-}
-
-void solve() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    vector<int> pos_1, pos_2, pos_3;
-
-   
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-        if (a[i] == 1) pos_1.push_back(i);
-        else if (a[i] == 2) pos_2.push_back(i);
-        else pos_3.push_back(i);
-    }
-
-    if (pos_1.empty() || pos_3.empty()) {
-        cout << "0\n";
-        return;
-    }
-
-    int total_count = 0;
-
-    
-    for (int i : pos_1) {
-        for (int j : pos_3) {
-            if (i < j) {
-             
-                int num_twos = upper_bound(pos_2.begin(), pos_2.end(), j - 1) -
-                               lower_bound(pos_2.begin(), pos_2.end(), i + 1);
-
-            
-                if (num_twos == 0) continue;
-
-                total_count = (total_count + (power(2, num_twos, MOD) - 1 + MOD) % MOD) % MOD;
-            }
-        }
-    }
-
-    cout << total_count << "\n";
-}
+#define testCase \
+    int t;       \
+    cin >> t;    \
+    while (t--)
 
 int32_t main() {
     faster();
-    testCase {
-        solve();
+    testCase{
+        int n;
+        cin>>n;
+        vi v(n);
+        input(v);
+        int cnt0=0,cnt1=0;
+        for(auto i:v)
+        {
+            if(i==0)
+            {
+                cnt0++;
+            }
+            else if(i==1){
+                cnt1++;
+            }
+
+        }
+
+            int x=(1ll<<cnt0)*cnt1;
+            cout<<x<<endl;
+        
     }
 }
