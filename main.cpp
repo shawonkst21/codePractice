2944fcc2a42fc9a72159f48e376a858b61182146
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2025-03-09 19:54:27 +0600

    day9

--- main.cpp
+++ main.cpp
@@ -1,65 +1,55 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl "\n"
+#define pb push_back
+const int N = 1e5 + 10;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+// int dx2[]={0,0,-1,1,1,1,-1,-1};
+// int dy2[]={-1,1,0,0,1,-1,1,-1};
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mii map<int, int>
+#define setBits(a) (int)__builtin_popcountll(a)
+#define mod 1000000007
+//priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
 
-int N;
-vector<vector<string>> result;
-
-bool isValid(int row, int col, vector<string>& board) {
-    for (int i = row - 1; i >= 0; i--) {
-        if (board[i][col] == 'Q') {
-            return false;
-        }
-    }
-    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-        if (board[i][j] == 'Q') {
-            return false;
-        }
-    }
-    for (int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++) {
-        if (board[i][j] == 'Q') {
-            return false;
-        }
-    }
-    return true;
+void faster() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 }
 
-void solve(vector<string>& board, int row) {
-    if (row == N) {
-        result.push_back(board);
-        return;
+#define input(a)      \
+    for (auto &x : a) \
+        cin >> x
+#define output(a)             \
+    {                         \
+        for (auto &x : a)     \
+            cout << x << ' '; \
+        cout << endl;         \
     }
-    for (int col = 0; col < N; col++) {
-        if (isValid(row, col, board)) {
-            board[row][col] = 'Q';
-            solve(board, row + 1);
-            board[row][col] = '.';
-        }
-    }
-}
+#define testCase \
+    int t;       \
+    cin >> t;    \
+    while (t--)
 
-void solveNQueens(int n) {
-    N = n;
-    vector<string> board(n, string(n, '.'));
-    solve(board, 0);
-}
+int32_t main() {
+    faster();
+   int n,k;
+   cin>>n>>k;
+   int l=240-k;
+   int cnt=0;
+   int time=0;
+   for(int i=5;i<=n*5;i+=5){
 
-void printSolutions() {
-    for (const auto& solution : result) {
-        for (const auto& row : solution) {
-            cout << row << endl;
-        }
-        cout << endl;
-    }
-}
-
-int main() {
-    int n;
-    cout << "Enter the value of N: ";
-    cin >> n;
-    solveNQueens(n);
-    cout << "Possible solutions:\n";
-    printSolutions();
-    return 0;
+         time+=i;
+         if(time<=l)
+         {
+            cnt++;
+         }
+   }
+   cout<<cnt<<endl;
 }
