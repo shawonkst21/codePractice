c9353e1a00e53f702443d4357e0f4a38e5c3024d
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2025-07-18 09:57:07 +0600

    after a long time

--- main.cpp
+++ main.cpp
@@ -39,15 +39,43 @@ void faster() {
 int32_t main() {
     faster();
     testCase{
-        int num;
-        cin>>num;
-        int x=sqrt(num);
-        if(x*x==num)
-        {
-            cout<<(1)<<" "<<x-1<<endl;
-        }
-        else{
-            cout<<"-1"<<endl;
-        }
+      int n,k;
+      cin>>n>>k;
+      vi v(n);
+      input(v);
+      vi count(n);
+      int temp=0;
+      for(int i=n-1;i>=0;i--)
+      {
+          if(v[i]==0)
+          {
+            temp++;
+            count[i]=temp;
+          }
+          else{
+            temp=0;
+          }
+      }
+      int ans=0;
+      int i=0;
+      while(i<n)
+      {
+          if(count[i]==0)
+          {
+            i++;
+          }
+          else{
+              if(count[i]>=k)
+              {
+                  ans++;
+                  i=i+k+1;
+              }
+              else{
+                i++;
+              }
+          }
+      }
+      cout<<ans<<endl;
+
     }
 }
